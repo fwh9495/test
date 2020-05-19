61af36661061cca6e9fa521a1d9adef627db1ab8
冯伟华 <fengweihua@fengweihuadeMBP.lan>
2020-05-19 17:15:19 +0800

    第一次提交

---
+++ .postcssrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "plugins": {
+      'autoprefixer': {
+        browsers: ['Android >= 4.0', 'iOS >= 7']
+      },
+      'postcss-pxtorem': {
+        rootValue: 37.5,
+        propList: ['*']
+      }
+    }
+  }
+  
+  
--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <img alt="Vue logo" src="./assets/logo.png" class="logo">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -24,5 +24,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.logo{
+  width: 40px;
+  height: 40px;
 }
 </style>
